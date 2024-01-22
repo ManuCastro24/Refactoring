6117a5eb41ae9a63ad637008c048e74779972f5c
Hugo <martin.romero.hugo@iescamas.es>
2024-01-22 17:43:21 +0100

    Ejercicios 1, 2 y 3 refactorizados

--- src/Refactoring/Ejercicio3.java
+++ src/Refactoring/Ejercicio3.java
@@ -5,47 +5,35 @@ import java.util.Scanner;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-
 		var sc = new Scanner(System.in);
-		
+
 		String frase;
-		
+
 		System.out.print("Introduce una frase : ");
 		frase = sc.nextLine();
-		
-		
-		TipodeCaracter(frase);
-		
 
-		
+		tipoDeCaracter(frase);
+
 	}
 
-	public static void TipodeCaracter(String frase) {
-		
-		int PosicionDelCaracter = 0; 
-		
-		
-		if(frase.length() / 2 == 0) {
-			
+	public static void tipoDeCaracter(String frase) {
+		int PosicionDelCaracter = 0;
+
+		if (frase.length() / 2 == 0) {
 			PosicionDelCaracter = (frase.length() / 2) - 1;
-			
-		}else {
-			
+
+		} else {
 			PosicionDelCaracter = (frase.length() / 2);
 		}
-		
+
 		char caracter = frase.charAt(PosicionDelCaracter);
-		
+
 		if (caracter == ' ') {
-			
 			System.out.print("Es un espacio");
-		
+
 		}
-		
-		if(caracter != ' '){
-			
+
+		if (caracter != ' ') {
 			System.out.print("No es un espacio");
 		}
 	}
